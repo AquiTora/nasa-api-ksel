83e37bc2d09d519d61e3ec62002d65c5eb84b394
Ksel Ivan <Kselivan98@gmail.com>
2023-08-17 13:23:15 +0500

    Страница астероида готова

---
+++ components/AsteroidPage/AsteroidPage.jsx
@@ -0,0 +1,16 @@
+import styles from './AsteroidPage.module.css';
+
+const AsteroidPage = ({ asteroid }) => {
+    console.log('asteroid', asteroid);
+    return (
+        <div className={styles.asteroidPage}>
+            <h1>{asteroid.name}</h1>
+            <p>Скорасть {asteroid.closeApproachData} км/ч</p>
+            <p>Время максимального сближения {asteroid.closeDistanceDate}</p>
+            <p>Расстояние до земли в километрах {asteroid.closeDistanceKilo}</p>
+            <p>Орбита: {asteroid.orbitingBody}</p>
+        </div>
+    )
+}
+
+export default AsteroidPage;
--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,6 +1,7 @@
 import { getAllAsteroidsId, getAsteroidData } from '../Service/NASAapi';
 import { useRouter } from 'next/router';
 import Logo from '../components/Logo/Logo';
+import AsteroidPage from '../components/AsteroidPage/AsteroidPage';
 
 export default function page(props) {
     const router = useRouter();
@@ -8,14 +9,9 @@ export default function page(props) {
     return (
         <div>
             <Logo />
-            <div>
-                <h1>{router.query.name}</h1>
-                <p>{router.query.closeDistanceDate}</p>
-                <p>{router.query.closeDistanceKilo}</p>
-                <p>{router.query.closeDistanceLunar}</p>
-                <p>{router.query.orbitingBody}</p>
-                <p>{router.query.size}</p>
-            </div>
+            <AsteroidPage 
+                asteroid={router.query}
+            />
         </div>
     )
 }
